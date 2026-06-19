1d4fc6e387c226c72258ec1b66682941e73c695e
lloydt888 <31457567+lloydt888@users.noreply.github.com>
2026-06-19 20:39:14 +1000

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
 // /assets/js/auth.js
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://rdwwpjfwsoufgbpihykn.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkd3dwamZ3c291ZmdicGloeWtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MzUyNTcsImV4cCI6MjA3MDQxMTI1N30.34g9cl9e3aep-iJEPkOgbkpHja07xRSv7QigfpyeiMI';
+const SUPABASE_URL = 'https://lwaaamofbppfbpntvera.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3YWFhbW9mYnBwZmJwbnR2ZXJhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTg1NDQ4NywiZXhwIjoyMDk3NDMwNDg3fQ.zAOqpidik6RpZkg26YdncANw-9ZNZHPNTu0tfFxWe3k';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
